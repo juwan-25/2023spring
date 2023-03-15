98e4de1549f808d956b5d93f4f0072e0defb6354
Juwan Lee <ljw05w5@gmail.com>
2023-03-15 12:19:19 +0900

    스프링 객체 생성하고 객체 주소 출력

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,5 +1,15 @@
-package kr.hs.study;public class Main {
+package kr.hs.study;
+
+import kr.hs.study.beans.TestBean1;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+
+        System.out.println("객체 주소 : "+obj1);
+
+        ctx.close();
     }
 }
--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -1,2 +1,9 @@
-package kr.hs.study.beans;public class TestBean1 {
+package kr.hs.study.beans;
+
+public class TestBean1 {
+    TestBean1(){
+        System.out.println("TestBean1의 기본생성자");
+    }
+    void init_method(){}
+    void destory_method(){}
 }
