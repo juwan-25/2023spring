f4e4edaeb81a19f6ad315356ef045d524eef28b7
Juwan Lee <ljw05w5@gmail.com>
2023-03-21 16:07:13 +0900

    Setter DI 활용하기

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,5 +1,23 @@
-package kr.hs.study;public class Main {
+package kr.hs.study;
+
+import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+        System.out.println(obj1.getData1());
+        System.out.println(obj1.getData2());
+        System.out.println(obj1.isData3());
+        System.out.println(obj1.getData4());
+
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println(obj2.getData1());
+        System.out.println(obj2.getData2());
+
+        ctx.close();
     }
 }
